f54dc82257d5fc4a05f64fe7ace70f447ea8e2b9
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2026-01-16 21:02:42 +0900

    feat: RotatableSpriteクラスの実装と描画試験

--- pitot-static-tube.ino
+++ pitot-static-tube.ino
@@ -10,61 +10,120 @@
 // ESP32でLovyanGFXを独自設定で利用
 class LGFX : public lgfx::LGFX_Device {
   lgfx::Panel_GC9A01 _panel_instance;
-  lgfx::Bus_SPI      _bus_instance;
+  lgfx::Bus_SPI _bus_instance;
 
 public:
   LGFX(void) {
-    { // バス制御の設定
-        auto cfg = _bus_instance.config(); // バス設定用の構造体を取得
+    {
+      // バス制御の設定
+      auto cfg = _bus_instance.config(); // バス設定用の構造体を取得
 
-        // SPIバスの設定
-        cfg.spi_host = SPI2_HOST;  // 使用するSPIを選択（ESP32-S2, C3: SPI2_HOST or SPI3_HOST）
-        cfg.spi_mode = 0;          // SPI通信モードを設定（0 ~ 3）
-        cfg.freq_write = 40000000; // 送信時のSPIクロック（最大80MHz, 80MHzを整数で割った値に丸められます）
-        cfg.freq_read = 16000000;  // 受信時のSPIクロック
-        cfg.spi_3wire = true;      // 受信をMOSIピンで行う場合はtrueを設定 = 受信線(MISO)を使わない設定？
-        cfg.use_lock = true;       // トランザクションロックを使用する場合はtrueを設定
-        cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定
-        cfg.pin_sclk = 8;
-        cfg.pin_mosi = 10;
-        cfg.pin_miso = -1; // -1 = disable
-        cfg.pin_dc = 5;
+      // SPIバスの設定
+      cfg.spi_host = SPI2_HOST;          // 使用するSPIを選択（ESP32-S2, C3: SPI2_HOST or SPI3_HOST）
+      cfg.spi_mode = 0;                  // SPI通信モードを設定（0 ~ 3）
+      cfg.freq_write = 40000000;         // 送信時のSPIクロック（最大80MHz, 80MHzを整数で割った値に丸められます）
+      cfg.freq_read = 16000000;          // 受信時のSPIクロック
+      cfg.spi_3wire = true;              // 受信をMOSIピンで行う場合はtrueを設定 = 受信線(MISO)を使わない設定？
+      cfg.use_lock = true;               // トランザクションロックを使用する場合はtrueを設定
+      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定
+      cfg.pin_sclk = 8;
+      cfg.pin_mosi = 10;
+      cfg.pin_miso = -1; // -1 = disable
+      cfg.pin_dc = 5;
 
-        _bus_instance.config(cfg); // 設定値をバスに反映
-        _panel_instance.setBus(&_bus_instance); // バスをパネルにセット
+      _bus_instance.config(cfg);              // 設定値をバスに反映
+      _panel_instance.setBus(&_bus_instance); // バスをパネルにセット
     }
 
-    { // 表示パネル制御の設定
-        auto cfg = _panel_instance.config(); // 表示パネル設定用の構造体を取得
+    {
+      // 表示パネル制御の設定
+      auto cfg = _panel_instance.config(); // 表示パネル設定用の構造体を取得
 
-        cfg.pin_cs           =     4; // CSが接続されているピン番号   (-1 = disable)
-        cfg.pin_rst          =     3; // RSTが接続されているピン番号  (-1 = disable)
-        cfg.pin_busy         =    -1; // BUSYが接続されているピン番号 (-1 = disable)
+      cfg.pin_cs = 4;    // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst = 3;   // RSTが接続されているピン番号  (-1 = disable)
+      cfg.pin_busy = -1; // BUSYが接続されているピン番号 (-1 = disable)
 
-        cfg.panel_width      =   240; // 実際に表示可能な幅
-        cfg.panel_height     =   240; // 実際に表示可能な高さ
-        cfg.offset_x         =     0; // パネルのX方向オフセット量
-        cfg.offset_y         =     0; // パネルのY方向オフセット量
-        cfg.offset_rotation  =     0; // 回転方向の値のオフセット 0~7 (4~7は上下反転)
-        cfg.dummy_read_pixel =     8; // ピクセル読出し前のダミーリードのビット数
-        cfg.dummy_read_bits  =     1; // ピクセル以外のデータ読出し前のダミーリードのビット数
-        cfg.readable         = false; // データ読出しが可能な場合 trueに設定
-        cfg.invert           =  true; // パネルの明暗が反転してしまう場合 trueに設定
-        cfg.rgb_order        = false; // パネルの赤と青が入れ替わってしまう場合 trueに設定
-        cfg.dlen_16bit       = false; // 16bitパラレルやSPIでデータ長を16bit単位で送信するパネルの場合 trueに設定
-        cfg.bus_shared       = false; // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+      cfg.panel_width = 240;    // 実際に表示可能な幅
+      cfg.panel_height = 240;   // 実際に表示可能な高さ
+      cfg.offset_x = 0;         // パネルのX方向オフセット量
+      cfg.offset_y = 0;         // パネルのY方向オフセット量
+      cfg.offset_rotation = 0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
+      cfg.dummy_read_pixel = 8; // ピクセル読出し前のダミーリードのビット数
+      cfg.dummy_read_bits = 1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
+      cfg.readable = false;     // データ読出しが可能な場合 trueに設定
+      cfg.invert = true;        // パネルの明暗が反転してしまう場合 trueに設定
+      cfg.rgb_order = false;    // パネルの赤と青が入れ替わってしまう場合 trueに設定
+      cfg.dlen_16bit = false;   // 16bitパラレルやSPIでデータ長を16bit単位で送信するパネルの場合 trueに設定
+      cfg.bus_shared = false;   // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
 
-        _panel_instance.config(cfg);
+      _panel_instance.config(cfg);
     }
 
     setPanel(&_panel_instance); // 使用するパネルをセット
   }
 };
 
+// 回転可能なスプライトを扱うクラス
+class RotatableSprite {
+private:
+  LGFX_Sprite _sprite;  // 針の画像を保持するスプライト
+  float _current_angle; // 現在の角度（度数法）
+
+  // static: クラス全体で1個のみ．
+  // constexpr: コンパイル時に値が決定する定数．ベタ書きの数値リテラルと同じで，命令に埋め込まれる．
+  static constexpr uint32_t TRANSPARENT = 0x000000; // 透過色を黒に設定
+
+public:
+  // コンストラクタ
+  RotatableSprite(LGFX* lgfx, int16_t w, int16_t h) // wとhはスプライト自身の大きさ
+    // 初期化リスト（代入ではなく生成時に初期化できる．）
+    : _sprite(lgfx),
+      _current_angle(0)
+  {
+    _sprite.setColorDepth(16);
+    _sprite.createSprite(w, h);
+    _sprite.setPivot(w >> 1, h >> 1); // スプライト自身の中央で回転
+  }
+
+  // 描画ロジックを関数として受け取る．
+  // 引数として「自分のスプライトのポインタ」を渡すことで，外部関数が書き込めるようにする．
+  using DrawCallBack = std::function<void(LGFX_Sprite*)>;
+
+  // スプライトを描画するメソッド
+  void createSpriteImage(DrawCallBack drawFunc) {
+    _sprite.fillScreen(TRANSPARENT); // 一旦クリア
+    if (drawFunc) {
+      drawFunc(&_sprite); // 注入された描画ロジックを実行
+    }
+  }
+
+  // 回転中心をずらすメソッド（デフォルトはスプライトの中央）
+  void setPivot(int16_t x, int16_t y) { _sprite.setPivot(x, y); }
+
+  // 角度の更新メソッド（即時反映）
+  void setAngle(float angle) {
+    _current_angle = angle - 90; // 上向きが0度
+  }
+
+  // メインキャンバスへの描画
+  void draw(LGFX_Sprite* canvas, int32_t x, int32_t y) {
+    _sprite.pushRotateZoom(
+      canvas,         // dst
+      x,              // dst_x
+      y,              // dst_y
+      _current_angle, // angle (deg)
+      1.0f,           // zoom_x
+      1.0f,           // zoom_y
+      TRANSPARENT     // transp
+    );
+  }
+};
+
 SensirionI2CSdp sdp; // センサのインスタンス
 
 LGFX lcd; // ディスプレイのインスタンス
-LGFX_Sprite sprite(&lcd); // スプライト（描画バッファ）
+LGFX_Sprite canvas(&lcd); // 描画バッファ
+RotatableSprite speed_pointer(&lcd, 24, 120);
 
 void setup() {
   Serial.begin(115200); // シリアル通信開始
@@ -97,12 +156,29 @@ void setup() {
   // 24の方がSPI通信量が多くなりますが，諧調表現が綺麗になります．
   lcd.setColorDepth(16);
 
-  // スプライト（画用紙）のメモリ確保
-  sprite.createSprite(240, 240);
-  // テキストサイズとフォント設定
-  sprite.setTextSize(2);
-  sprite.setTextColor(TFT_WHITE);
-  // sprite.setFont(&fonts::Orbitron_Light_24); // カッコいいフォント例
+  canvas.createSprite(lcd.width(), lcd.height());
+  canvas.setColorDepth(16);
+
+  speed_pointer.setPivot(12, 120);
+  speed_pointer.createSpriteImage([](LGFX_Sprite* sp) {
+    int w = sp->width();
+    int h = sp->height();
+
+    // 三角形
+    sp->fillTriangle(
+      w / 2, 0,
+      w, 20,
+      0, 20,
+      sp->color888(255, 255, 255)
+    );
+
+    // センターライン
+    sp->drawLine(
+      w / 2, 0,
+      w / 2, h,
+      sp->color888(255, 255, 255)
+    );
+  });
 }
 
 void loop() {
@@ -146,29 +222,11 @@ void loop() {
   }
 
   // --- 描画処理 ---
+  canvas.fillScreen(lcd.color888(255, 0, 0));
 
-  // 1. 背景を塗りつぶす（消去）
-  // sprite.fillScreen(lcd.color888(255, 0, 0)); // 綺麗な赤色
-  sprite.fillScreen(lcd.color888(0, 255, 0)); // 黄緑？
-  // sprite.fillScreen(lcd.color888(0, 0, 255)); // 綺麗な青色
+  speed_pointer.draw(&canvas, canvas.width() >> 1, canvas.height() >> 1); // 回転中心pivotの座標を指定
 
-  // 2. デザインを描く
-  // 外周のリング
-  sprite.drawCircle(120, 120, 118, lcd.color888(0, 0, 255));
-  sprite.drawCircle(120, 120, 117, lcd.color888(0, 0, 255));
-  
-  // 中心に数値を表示
-  sprite.setCursor(90, 100); // 位置合わせ
-  sprite.printf("%4.2f", real_temperature);
-  
-  // 単位
-  sprite.setCursor(90, 130);
-  sprite.setTextSize(1);
-  sprite.print("C");
-  sprite.setTextSize(2); // 戻す
+  canvas.pushSprite(0, 0); // 転送
 
-  // 3. 画面に転送（ここで初めて表示される）
-  sprite.pushSprite(0, 0);
-
-  delay(100); // 10Hz
+  delay(100);  // 10Hz
 }
